1d07fe05f3041f1ccb4c112b62c2267ab362ce7d
Yi <yi@YideMac-mini.local>
2018-04-15 21:52:42 +0800

    Initial Commit

---
+++ Videos/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Videos
+//
+//  Created by Yi on 2018/4/15.
+//  Copyright © 2018年 Yi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Videos/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Videos
+//
+//  Created by Yi on 2018/4/15.
+//  Copyright © 2018年 Yi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
